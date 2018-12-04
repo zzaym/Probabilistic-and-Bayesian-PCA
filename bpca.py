c934624380aefb6a3fb19e401abf952e0f6f522a
Duuuuuu <djzdevin@foxmail.com>
2018-12-03 22:27:57 -0500

    fix bugs of bpca

--- bpca.py
+++ bpca.py
@@ -3,7 +3,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class BPCA(object):
 
     def __init__(self, X, a_alpha=1e-3, b_alpha=1e-3, \
@@ -12,6 +11,7 @@ class BPCA(object):
         self.X = X.reshape(-1,1) if len(X.shape) < 2 else X
         self.d = self.X.shape[0]
         self.N = self.X.shape[1]
+        self.q = self.d-1
 
         # hyperparameters
         self.a_alpha = a_alpha
@@ -21,21 +21,21 @@ class BPCA(object):
         self.beta = beta
 
         # variational parameters
-        self.mean_z = np.random.randn(self.d, self.N) # latent variable
-        self.cov_z = np.random.randn(self.d, self.d)
+        self.mean_z = np.random.randn(self.q, self.N) # latent variable
+        self.cov_z = np.eye(self.q)
         self.mean_mu = np.random.randn(self.d, 1)
-        self.cov_mu = np.random.randn(self.d, self.d)
-        self.mean_w = np.random.randn(self.d, self.d)
-        self.cov_w = np.random.randn(self.d, self.d)
+        self.cov_mu = np.eye(self.d)
+        self.mean_w = np.random.randn(self.d, self.q)
+        self.cov_w = np.eye(self.d)
         self.a_alpha_tilde = self.a_alpha + self.d/2
-        self.b_alpha_tilde = np.abs(np.random.randn(self.d, 1))
+        self.b_alpha_tilde = np.abs(np.random.randn(self.q, 1))
         self.a_tau_tilde = self.a_tau + self.N * self.d / 2
         self.b_tau_tilde = np.abs(np.random.randn(1))
 
     def update(self):
         self.tau = self.a_tau_tilde / self.b_tau_tilde
         self.alpha = (self.a_alpha_tilde / self.b_alpha_tilde).flatten()
-        self.cov_z = np.linalg.inv(np.eye(self.d) + self.tau *
+        self.cov_z = np.linalg.inv(np.eye(self.q) + self.tau *
                         (np.trace(self.cov_w) + np.dot(self.mean_w.T, self.mean_w)))
         self.mean_z = self.tau * np.dot(np.dot(self.cov_z, self.mean_w.T), self.X - self.mean_mu)
         self.cov_mu = np.eye(self.d) / (self.beta + self.N * self.tau)
@@ -43,9 +43,9 @@ class BPCA(object):
                         self.mean_z), axis=1)).reshape(self.d, 1)
         self.cov_w = np.linalg.inv(np.diag(self.alpha) + self.tau *
                         (self.N * self.cov_z + np.dot(self.mean_z, self.mean_z.T)))
-        self.mean_w = self.tau * np.dot(self.cov_w, np.dot(self.mean_z, (self.X-self.mean_mu).T))
+        self.mean_w = self.tau * np.dot(self.cov_w, np.dot(self.mean_z, (self.X-self.mean_mu).T)).T
         self.b_alpha_tilde = self.b_alpha + (np.trace(self.cov_w) +
-                        np.array([np.dot(self.mean_w[:,i], self.mean_w[:,i]) for i in range(self.d)])) / 2
+                        np.array([np.dot(self.mean_w[:,i], self.mean_w[:,i]) for i in range(self.q)])) / 2
         self.b_tau_tilde = self.b_tau + 0.5 * np.sum(np.array([
                         np.dot(self.X[:,i], self.X[:,i]) +
                         np.trace(self.cov_mu) + np.dot(self.mean_mu.flatten(), self.mean_mu.flatten()) +
@@ -65,7 +65,7 @@ class BPCA(object):
     def hinton(self, matrix=None, max_weight=None, ax=None):
         """Draw Hinton diagram for visualizing a weight matrix."""
         if matrix is None:
-            matrix = self.mean_w
+            matrix = self.mean_w.T
             # matrix = np.zeros_like(self.mean_w)
             # for i in range(self.d):
             #     matrix[:,i] = np.random.multivariate_normal(self.mean_w[:,i], self.cov_w)
@@ -90,9 +90,3 @@ class BPCA(object):
         ax.invert_yaxis()
         plt.show()
 
-
-cov = np.diag(np.array([5,4,3,2,1,1,1,1,1,1])**2)
-data = np.random.multivariate_normal(np.zeros(10), cov, size=100)
-bpca = BPCA(data.T)
-bpca.fit()
-bpca.hinton()
