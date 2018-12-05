af87db4d82058a646094c9f312f7e018722bb6c2
Chen Feiyu <chenfeiyu@Chens-MacBook-Pro.local>
2018-12-05 15:13:01 -0500

    Added functions to implement.

--- ppca.py
+++ ppca.py
@@ -1,5 +1,17 @@
 """
-Probablistic Principal Component Analysis from Tipping & Bishop 1997
-See http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.426.2749&rep=rep1&type=pdf
+Probablistic Principal Component Analysis using the EM algorithm from Tipping & Bishop 1997.
+(See http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.426.2749&rep=rep1&type=pdf)
 """
 
+class PPCA(object):
+
+    def __init__(self, data, n_dimension):
+        self._X = data
+        self._q = n_dimension
+        pass
+    
+    def fit(n_iteration):
+        pass
+    
+    def generate(n_sample):
+        pass
