4734762cee0432c4e798d9c341dd5895269b3b7c
Chen Feiyu <chenfeiyu@Chens-MacBook-Pro.local>
2018-12-05 21:04:00 -0500

    Updated demo

--- ppca_demo.py
+++ ppca_demo.py
@@ -4,22 +4,23 @@ from numpy.random       import multivariate_normal
 from ppca               import PPCA
 
 if __name__ == '__main__':
-    cov  = np.diag([5, 4, 3, 2, 1, 1, 1, 1, 1, 1])**2
+    cov  = np.diag([10] + [1]*31 + [10] + [1]*31)**2
+    data = multivariate_normal(np.zeros(64), cov, 1000)
+    
+    ppca = PPCA(n_dimension=2)
+    ppca.fit(data)
+    
     plt.matshow(cov);
     plt.title('original covariance matrix')
     plt.show()
-    data = multivariate_normal(np.zeros(10), cov, 1000)
-    plt.scatter(data[:, 0], data[:, 1], alpha=0.2);
-    plt.title('original data (first 2 dimension)')
-    plt.show()
-    
-    
-    ppca = PPCA(n_dimension=4)
-    ppca.fit(data)
-    gene = ppca.generate(1000)
     plt.matshow(ppca._C);
     plt.title('fitted covariance matrix')
     plt.show()
+    
+    plt.scatter(data[:, 0], data[:, 1], alpha=0.2);
+    plt.title('original data (first 2 dimensions)')
+    plt.show()
+    gene = ppca.generate(1000)
     plt.scatter(gene[:, 0], gene[:, 1], alpha=0.2);
-    plt.title('generated data (first 2 dimension)')
+    plt.title('generated data (first 2 dimensions)')
     plt.show()
