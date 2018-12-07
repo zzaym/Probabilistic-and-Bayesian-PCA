bff20b2023c7e3f094ae83de95ce0ccea808921c
Chen Feiyu <chenfeiyu@Chens-MacBook-Pro.local>
2018-12-07 18:43:01 -0500

    Added method for fitting by eigenvalue decomposition

--- ppca.py
+++ ppca.py
@@ -3,9 +3,10 @@ Probablistic Principal Component Analysis using the EM algorithm from Tipping & 
 (See http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.426.2749&rep=rep1&type=pdf)
 """
 
-import numpy             as np
+import numpy as np
 from numpy.random          import randn
 from numpy.random          import multivariate_normal
+from numpy.linalg          import eig
 from numpy.linalg          import pinv
 from numpy.linalg          import multi_dot
 from sklearn.exceptions    import NotFittedError
@@ -19,7 +20,7 @@ class PPCA(object):
         # q: dimension of latent space
         self._q = n_dimension 
     
-    def fit(self, data, n_iteration=500):
+    def fit(self, data, n_iteration=500, method='EM'):
         
         ####################### INITIALIZE OBSERVATIONS #######################
         # X: observations, X in R^(d*N), data assumed to be in R^(N*d)
@@ -28,6 +29,25 @@ class PPCA(object):
         self._d = self._X.shape[0]
         # N: number of observations
         self._N = self._X.shape[1]
+        
+        if   method == 'EM':
+            self._fit_EM(data, n_iteration)
+        elif method == 'eig':
+            self._fit_eig(data) 
+        else:
+            raise ValueError('unrecognized method.')
+            
+        # C: covariance matrix of observation, x ~ N(mu, C)
+        self._C = self._sigma2 * np.eye(self._d) + np.dot(self._W, self._W.T)       
+    
+    def generate(self, n_sample):
+        try:
+            return multivariate_normal(self._mu.flatten(), self._C, n_sample)
+        except:
+            raise NotFittedError('This PPCA instance is not fitted yet. Call \'fit\' with appropriate arguments before using this method.')
+        
+    ######################## FITTING BY EM ALGORITHM ##########################
+    def _fit_EM(self, data, n_iteration=500):
         
         ##################### INITIALIZE LATENT VARIABLES #####################     
         # W: linear transformation matrix, W in R^(d*q)
@@ -43,16 +63,7 @@ class PPCA(object):
             # E-step: Estimation   (omitted)
             # M-step: Maximization
             self._maximize_L()
-        # C: covariance matrix of observation, x ~ N(mu, C)
-        self._C = self._sigma2 * np.eye(self._d) + np.dot(self._W, self._W.T)
     
-    def generate(self, n_sample):
-        try:
-            return multivariate_normal(self._mu.flatten(), self._C, n_sample)
-        except:
-            raise NotFittedError('This PPCA instance is not fitted yet. Call \'fit\' with appropriate arguments before using this method.')
-        
-    ################## SUBROUTINES OF M-STEP: MAXIMIZATION ####################
     def _maximize_L(self):
         S = self._calculate_S()
         M = self._calculate_M()
@@ -68,9 +79,27 @@ class PPCA(object):
         temp = multi_dot([ S, self._W, pinv(M), self._W.T ])
         self._sigma2 = 1/self._d * np.trace(S - temp)
     
+    ##################### FITTING BY EIGENDECOMPOSITION #######################
+    def _fit_eig(self, data, n_iteration=500):
+        
+        self._mu = np.average(self._X, axis=1).reshape(-1, 1)
+        S = self._calculate_S()
+        vals, vecs = eig(S)
+        ordbydom = np.argsort(vals)[::-1]
+        topq_dom = ordbydom[:self._q]
+        less_dom = ordbydom[self._q:]
+        self._sigma2 = np.sum(vals[less_dom]) / (self._d - self._q)
+        self._W = np.dot( vecs[:, topq_dom], 
+                          np.sqrt(np.diag(vals[topq_dom])-self._sigma2*np.eye(self._q)) )
+        # eig, sort, sigma
+        return # todo
+    
+    ########################### UTILITY FUNCTIONS #############################
     def _calculate_S(self):
         centeredX = self._X - self._mu
         return np.dot(centeredX, centeredX.T) / self._N
     
     def _calculate_M(self):
         return self._sigma2 * np.eye(self._q) + np.dot(self._W.T, self._W)
+        
+        
