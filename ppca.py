990e709c1bc2f7db5464f9c3072e1b5d7dce5495
Feiyu <feiyu_chen@g.harvard.edu>
2018-12-09 13:29:48 -0500

    Added log-likelihood support in PPCA.

--- ppca.py
+++ ppca.py
@@ -6,6 +6,7 @@ Probablistic Principal Component Analysis using the EM algorithm from Tipping & 
 import numpy as np
 from numpy.random          import randn
 from numpy.random          import multivariate_normal
+from numpy.linalg          import det
 from numpy.linalg          import eig
 from numpy.linalg          import pinv
 from numpy.linalg          import multi_dot
@@ -20,7 +21,10 @@ class PPCA(object):
         # q: dimension of latent space
         self._q = n_dimension 
     
-    def fit(self, data, n_iteration=500, method='EM'):
+    def fit(self, data, n_iteration=500, method='EM', keep_likelihoods=False):
+        
+        if method == 'eig' and keep_likelihoods: 
+            raise ValueError('Likelihood not supported for eig method') 
         
         ####################### INITIALIZE OBSERVATIONS #######################
         # X: observations, X in R^(d*N), data assumed to be in R^(N*d)
@@ -30,15 +34,18 @@ class PPCA(object):
         # N: number of observations
         self._N = self._X.shape[1]
         
+        likelihoods = []
         if   method == 'EM':
-            self._fit_EM(data, n_iteration)
+            likelihoods = self._fit_EM(data, n_iteration, keep_likelihoods)
         elif method == 'eig':
             self._fit_eig(data) 
         else:
             raise ValueError('unrecognized method.')
             
         # C: covariance matrix of observation, x ~ N(mu, C)
-        self._C = self._sigma2 * np.eye(self._d) + np.dot(self._W, self._W.T)       
+        self._C = self._sigma2 * np.eye(self._d) + np.dot(self._W, self._W.T)  
+        
+        return likelihoods
     
     def generate(self, n_sample):
         try:
@@ -47,7 +54,7 @@ class PPCA(object):
             raise NotFittedError('This PPCA instance is not fitted yet. Call \'fit\' with appropriate arguments before using this method.')
         
     ######################## FITTING BY EM ALGORITHM ##########################
-    def _fit_EM(self, data, n_iteration=500):
+    def _fit_EM(self, data, n_iteration=500, keep_likelihoods=False):
         
         ##################### INITIALIZE LATENT VARIABLES #####################     
         # W: linear transformation matrix, W in R^(d*q)
@@ -59,10 +66,15 @@ class PPCA(object):
         # epsilon: Gaussian noise, epsilon in R^(d), epsilon ~ N(0, sigma^2 I)
         self._sigma2 = np.abs(randn())
         
+        likelihoods =[]
         for i in range(n_iteration):
             # E-step: Estimation   (omitted)
             # M-step: Maximization
             self._maximize_L()
+            if keep_likelihoods:
+                likelihoods.append(self._calc_likelihood())
+        
+        return likelihoods
     
     def _maximize_L(self):
         S = self._calculate_S()
@@ -78,6 +90,14 @@ class PPCA(object):
     def _update_sigma2(self, S, M):
         temp = multi_dot([ S, self._W, pinv(M), self._W.T ])
         self._sigma2 = 1/self._d * np.trace(S - temp)
+    
+    def _calc_likelihood(self):
+        C = self._sigma2 * np.eye(self._d) + np.dot(self._W, self._W.T)
+        S = self._calculate_S()
+        return -self._N*self._d/2 * np.log(2*np.pi) \
+               -self._N/2 * np.log(det(C)) \
+               -self._N/2 * np.trace(np.dot( pinv(C), S ))
+    
     
     ##################### FITTING BY EIGENDECOMPOSITION #######################
     def _fit_eig(self, data, n_iteration=500):
