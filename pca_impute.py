b5768cb945b0e6564b68415bfbf4267a650b83b7
Feiyu <feiyu_chen@g.harvard.edu>
2018-12-09 17:46:54 -0500

    Used ppca for impute

--- pca_impute.py
+++ pca_impute.py
@@ -1,5 +1,5 @@
-import numpy               as np
-from sklearn.decomposition import PCA # should be changed to PPCA later
+import numpy as np
+from ppca    import PPCA
 
 class PCAImputer:
     
@@ -10,14 +10,14 @@ class PCAImputer:
         self._data     = data.copy() 
         self._missing  = np.isnan(data)
         self._observed = ~self._missing
-        self._pca      = PCA(n_components = self._q)
+        self._pca      = PPCA(n_dimension = self._q)
         
         row_defau = np.zeros(self._data.shape[0])
         row_means = np.repeat(np.nanmean(self._data, axis=1, out=row_defau).reshape(-1, 1), \
                               self._data.shape[1], axis=1)
         self._data[self._missing] = row_means[self._missing]
         for i in range(n_iteration):
-            self._pca.fit(self._data)
+            self._pca.fit(self._data, method='eig')
             self._data[self._missing] = self._pca.inverse_transform( \
                                         self._pca.transform(self._data) )[self._missing]
         return self._data
