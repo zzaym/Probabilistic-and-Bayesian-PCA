116bd46571026ed57d521f91498de6638263c13a
Duuuuuu <djzdevin@foxmail.com>
2018-12-12 01:28:37 -0500

    bpca transform fixed and general pca imputer

--- bpca.py
+++ bpca.py
@@ -47,7 +47,7 @@ class BPCA(object):
 
     def calculate_log_likelihood(self):
         w = self.mean_w
-        c = np.eye(self.d)/self.tau + np.dot(w, w.T) 
+        c = np.eye(self.d)*self.tau + np.dot(w, w.T) 
         xc = self.X - self.X.mean(axis=1).reshape(-1,1)
         s = np.dot(xc, xc.T) / self.N
         self.s = s
@@ -161,20 +161,30 @@ class BPCA(object):
         self.ed = np.array([i for i, inv_alpha in enumerate(1/self.alpha) if inv_alpha > sum_alpha/self.q])
 
 
-    def transform(self, X=None):
+    def transform(self, X=None, full=True):
         X = self.X if X is None else X.T
-        w = self.mean_w[:, self.ed]
-        m = np.eye(len(self.ed))/self.tau + np.dot(w.T, w)
+        if full:
+            w = self.mean_w
+            l = self.q
+        else:
+            w = self.mean_w[:,ed]
+            l = len(self.ed)
+        m = np.eye(l)*self.tau + np.dot(w.T, w)
         inv_m = np.linalg.inv(m)
         z = np.dot(np.dot(inv_m, w.T), X - self.mean_mu)
-        return np.array([np.random.multivariate_normal(z[:,i], inv_m/self.tau) for i in range(X.shape[1])])
+        return z.T
+        # return np.array([np.random.multivariate_normal(z[:,i], inv_m*self.tau) for i in range(X.shape[1])])
 
 
-    def inv_transform(self, z):
+    def inverse_transform(self, z, full=True):
         z = z.T
-        w = self.mean_w[:, self.ed]
+        if full:
+            w = self.mean_w
+        else:
+            w = self.mean_w[:,ed]
         x = np.dot(w, z) + self.mean_mu
-        return np.array([np.random.multivariate_normal(x[:,i], np.eye(self.d)/self.tau) for i in range(z.shape[1])])
+        return x.T
+        # return np.array([np.random.multivariate_normal(x[:,i], np.eye(self.d)*self.tau) for i in range(z.shape[1])])
 
 
     def fit_transform(self, X=None, batch_size=128, iters=500, print_every=100, verbose=False, trace_elbo=False, trace_loglikelihood=False):
@@ -184,7 +194,7 @@ class BPCA(object):
 
     def generate(self, size=1):
         w = self.mean_w[:, self.ed]
-        c = np.eye(self.d)/self.tau + np.dot(w, w.T)
+        c = np.eye(self.d)*self.tau + np.dot(w, w.T)
         return np.array([np.random.multivariate_normal(self.mean_mu.flatten(), c) for i in range(size)])
 
 
@@ -202,7 +212,7 @@ class BPCA(object):
 
     def get_cov_mat(self):
         w = self.mean_w[:, self.ed]
-        c = np.eye(self.d)/self.tau + np.dot(w, w.T) 
+        c = np.eye(self.d)*self.tau + np.dot(w, w.T) 
         return c
 
 
---
+++ pca_all_impute.py
@@ -0,0 +1,43 @@
+import numpy as np
+from ppca import PPCA
+from sklearn.decomposition import PCA
+from bpca import BPCA
+
+class PCAImputer:
+    
+    def __init__(self, method='pca', n_dimension=0):
+        self._q = n_dimension
+        self._method = method
+        if method == 'pca':
+            self._pca = PCA(n_components=self._q)
+        elif method == 'ppca':
+            self._pca = PPCA(n_dimension=self._q)
+        else:
+            self._pca = BPCA()
+    
+    def fit_transform(self, data, ppca_method='eig', probabilistic=False, n_iteration=100, \
+                        verbose=False, print_every=10, trace_mse=False, cdata=None):
+        self._data     = data.copy() 
+        self._missing  = np.isnan(data)
+        self._observed = ~self._missing
+        self._mse = np.zeros(n_iteration)
+              
+        row_defau = np.zeros(self._data.shape[0])
+        row_means = np.repeat(np.nanmean(self._data, axis=1, out=row_defau).reshape(-1, 1), \
+                              self._data.shape[1], axis=1)
+        self._data[self._missing] = row_means[self._missing]
+
+        for i in range(n_iteration):
+            if self._method == 'ppca':           
+                self._pca.fit(self._data, method=ppca_method)
+                self._data[self._missing] = self._pca.inverse_transform(self._pca.transform(self._data, \
+                                            probabilistic), probabilistic)[self._missing]
+            else:
+                self._pca.fit(self._data)
+                self._data[self._missing] = self._pca.inverse_transform(self._pca.transform(self._data))[self._missing]
+            self._mse[i] = np.sum((cdata-self._data)**2)/cdata.shape[0]
+            if verbose and i % print_every == 0:
+                print('Iter %d, MSE=%f' %(i, self._mse[i]))
+            if np.abs(self._mse[i-1]-self._mse[i]) < 1e-3:
+                break
+        return self._data, self._mse
