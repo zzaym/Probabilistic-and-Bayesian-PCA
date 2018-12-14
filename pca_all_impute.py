2e1f7b351103edc4271a99ac3639724167909146
Duuuuuu <djzdevin@foxmail.com>
2018-12-13 23:24:50 -0500

    Toy imputation test and minor fix of imputer

--- pca_all_impute.py
+++ pca_all_impute.py
@@ -6,8 +6,6 @@ from bpca import BPCA
 class PCAImputer:
     
     def __init__(self, method='pca', n_dimension=0):
-        """There are three kind of PCA supported for this imputer: conventional
-        PCA, probabilistic PCA and Bayesian PCA."""
         self._q = n_dimension
         self._method = method
         if method == 'pca':
@@ -18,10 +16,7 @@ class PCAImputer:
             self._pca = BPCA()
     
     def fit_transform(self, data, ppca_method='eig', probabilistic=False, n_iteration=100, \
-                        verbose=False, print_every=10, trace_mse=False, cdata=None, tol=1e-3):
-        """fitting a PCA to the original data by iterativly filling the missing entries
-        with value generated from PCA. Each missing entries are initialized with the
-        row mean."""
+                        verbose=False, print_every=50, trace_mse=False, cdata=None):
         self._data     = data.copy() 
         self._missing  = np.isnan(data)
         self._observed = ~self._missing
@@ -31,6 +26,7 @@ class PCAImputer:
         row_means = np.repeat(np.nanmean(self._data, axis=1, out=row_defau).reshape(-1, 1), \
                               self._data.shape[1], axis=1)
         self._data[self._missing] = row_means[self._missing]
+        self._data = np.nan_to_num(self._data)
 
         for i in range(n_iteration):
             if self._method == 'ppca':           
@@ -40,9 +36,9 @@ class PCAImputer:
             else:
                 self._pca.fit(self._data)
                 self._data[self._missing] = self._pca.inverse_transform(self._pca.transform(self._data))[self._missing]
-            # self._mse[i] = np.sum((cdata-self._data)**2)/cdata.shape[0]
+            self._mse[i] = np.sum((cdata-self._data)**2)/cdata.shape[0]
             if verbose and i % print_every == 0:
                 print('Iter %d, MSE=%f' %(i, self._mse[i]))
-            if np.abs(self._mse[i-1]-self._mse[i]) < tol:
-                break
+            # if np.abs(self._mse[i-1]-self._mse[i]) < 1e-6:
+            #     break
         return self._data, self._mse
